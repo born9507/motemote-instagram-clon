65a3059c3eecd63ee15a928bb118de7b6247dc6a
Sangwon <born9507@snu.ac.kr>
2021-12-03 22:55:24 +0900

    [fix] Fix!

--- accounts/static/accounts/js/signup.js
+++ accounts/static/accounts/js/signup.js
@@ -14,10 +14,56 @@ const handleSignup = (e) => {
 
   if (validataUsername(username) && validatePassword(passwords)) {
     console.log('Valid signup form!')
+    submitTarget(e);
   } else {
     console.log('Invalid!')
+    dismissSignup();
   }
 }
+
+const dismissSignup = () => {
+  showErrorModal();
+  setGrayBackGroundColor();
+  disableInputTags();
+}
+
+const errorModal = document.getElementById('signup-error-modal');
+const showErrorModal = () => {
+  errorModal.classList.add('show');
+}
+
+const modalCloseBtn = document.querySelector('.modal-header > .close-button');
+modalCloseBtn.onclick = () => handleClose();
+
+const handleClose = () => {
+  hideErrorModal();
+  resetGrayBackGroundColor();
+  enableInputTags();
+}
+
+const hideErrorModal = () => {
+  errorModal.classList.remove('show')
+}
+
+const bodyTag = document.querySelector('body');
+
+const setGrayBackGroundColor = () => {
+  bodyTag.classList.add('gray');
+}
+
+const resetGrayBackGroundColor = () => {
+  bodyTag.classList.remove('gray');
+}
+
+const inputTags = [...document.querySelectorAll('input')];
+
+const disableInputTags = () => {
+	inputTags.forEach((inputTag) => (inputTag.disabled = true));
+};
+
+const enableInputTags = () => {
+	inputTags.forEach((inputTags) => (inputTags.disabled = false));
+};
 
 const getUsername = () => {
   return document.querySelector('input[name=username]').value;
@@ -36,9 +82,19 @@ const getPasswords = () => {
 }
 
 const validatePassword = (passwords) => {
-  return isSamePassword(passwords);
+  return isSamePassword(passwords) && isValidFormatPassword(passwords);
 }
+
+const isValidFormatPassword = ([pw]) => {
+	const regExp = /^[A-Za-z0-9]{3,}$/;
+
+	return regExp.test(pw);
+};
 
 const isSamePassword = ([pw1, pw2]) => {
   return pw1 === pw2;
+}
+
+const submitTarget = (e) => {
+  e.target.submit();
 }
